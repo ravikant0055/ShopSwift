7cb4210c02ba7801977f66b6ac7adc3f4944c320
Ravi Kant <Kumarra927@gmail.com>
2024-01-18 10:33:14 +0530

    Navbar Update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Navbar from './components/common/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
       <span className='text-3xl'>Abhishek</span>
+      <Navbar/>
     </div>
   );
 }
