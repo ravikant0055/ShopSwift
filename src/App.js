45b68b7b5438767d72e1ace4a5c0071ea7477e1f
Ravi Kant <Kumarra927@gmail.com>
2024-01-21 14:49:28 +0530

    code update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import Login from './pages/Login';
 import { Collection } from './components/common/content/Collection';
 import Header from './components/common/header/Header';
 import Navbar from './components/common/navbar/Navbar';
