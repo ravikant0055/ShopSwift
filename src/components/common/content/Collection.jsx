b9b9e8c09624927b28a38356d382b425898c9402
Praveen Rohilla -X (prarohil - WIPRO LIMITED at Cisco) <prarohil@cisco.com>
2024-01-21 13:50:39 +0530

    Header work has been completed

---
+++ src/components/common/content/Collection.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const Collection = () => {
+  return (
+    <div className=' h-[440px] mb-10 sm:mb-20 md:mb-30 flex flex-col sm:flex-row justify-between items-center'>
+        <div  className='mb-4 sm:mb-0'>
+            <img className='max-w-full h-auto sm:max-h-[330px]'  src={process.env.PUBLIC_URL + '/images/men.png'} alt='men_image' />
+        </div>
+        <div>
+            <img className='max-w-full h-auto sm:max-h-[330px]' src={process.env.PUBLIC_URL + '/images/female.png'} alt="female image"/>
+        </div>
+    </div>
+  )
+
+
+
+}
+
+
+
+
+
