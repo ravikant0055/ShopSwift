c6df8c26cf0cca50c1e9694d7ca3d3a15e505abd
Praveen Rohilla -X (prarohil - WIPRO LIMITED at Cisco) <prarohil@cisco.com>
2024-01-29 11:53:57 +0530

    again pulling

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Routes from './routes';
 
 import Trending from './components/common/trending/Trending';
+import Navbar from './components/common/navbar/Navbar';
+import Header from './components/common/header/Header';
+import { Collection } from './components/common/content/Collection';
+import { Categories } from './components/common/category/Categories';
+import Footer from './components/common/footer/Footer';
 function App() {
   return (
    <Router>
