42940bab816db7c61c4e2dc846626ae2998dc873
Ravi Kant <Kumarra927@gmail.com>
2024-02-01 12:54:18 +0530

    conflit error slove

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import './App.css';
-<<<<<<< HEAD
-import { Categories } from './components/common/category/Categories';
 import Home from './pages/Home';
 import Men from './pages/Men';
 import Female from './pages/Women';
@@ -9,36 +7,12 @@ import Cart from './pages/Cart';
 import Demo from './pages/Demo';
 import Navbar from './components/common/navbar/Navbar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-<<<<<<< HEAD
 import Login from './pages/Login';
-import { Collection } from './components/common/content/Collection';
-=======
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Routes from './routes';
-
-import Trending from './components/common/trending/Trending';
-import Navbar from './components/common/navbar/Navbar';
->>>>>>> c6df8c26cf0cca50c1e9694d7ca3d3a15e505abd
-import Header from './components/common/header/Header';
-import { Collection } from './components/common/content/Collection';
-import { Categories } from './components/common/category/Categories';
-import Footer from './components/common/footer/Footer';
-=======
 import ProductItems from './pages/ProductItems';
->>>>>>> Ravi
 function App() {
   return (
    <Router>
     <div className="App">
-<<<<<<< HEAD
-      
-      <Navbar /> 
-      <Header />
-      <Collection />
-      <Categories/>
-      <Footer />
-
-=======
     <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>} />
@@ -50,7 +24,6 @@ function App() {
         <Route path='/demo' element={<Demo/>}/>
         <Route path='/productitems' element={<ProductItems/>}/>
       </Routes>
->>>>>>> Ravi
     </div>
    </Router> 
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,16 @@
 import React from 'react'
 import { Collection } from '../components/common/content/Collection';
 import Header from '../components/common/header/Header';
+import Footer from '../components/common/footer/Footer'
 import { Categories } from '../components/common/category/Categories';
 
 const Home = () => {
   return (
     <div>
-<<<<<<< HEAD
       <Header/>
-<<<<<<< HEAD
-      <Collection/>
-=======
->>>>>>> Ravi
-      <Footer/>
-=======
-      <Header />
       <Collection/>
       <Categories />
->>>>>>> c6df8c26cf0cca50c1e9694d7ca3d3a15e505abd
+      <Footer/>
     </div>
   )
 }
