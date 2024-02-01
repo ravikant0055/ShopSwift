81d597990060d2d31748c1248b24b1337d56e316
Abhishek <abhishekahuja576@gmail.com>
2024-02-01 10:40:45 +0530

    imported elements

--- src/App.js
+++ src/App.js
@@ -6,32 +6,13 @@ import Kids from './pages/Kids';
 import Cart from './pages/Cart';
 import Demo from './pages/Demo';
 import Navbar from './components/common/navbar/Navbar';
-<<<<<<< HEAD
-import Header from './components/common/header/Header';
-import { Collection } from './components/common/content/Collection';
-import { Categories } from './components/common/category/Categories';
-import Footer from './components/common/footer/Footer';
-import ProductDetail from './components/common/ProductDetails/ProductDetail';
-=======
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import ProductItems from './pages/ProductItems';
->>>>>>> origin/master
 function App() {
   return (
    <Router>
     <div className="App">
-<<<<<<< HEAD
-      
-      <Navbar /> 
-      {/* <Header />
-      <Collection />
-      <Categories/>
-      <Trending /> */}
-      {/* <Footer /> */}
-      <ProductDetail />
-
-=======
     <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>} />
@@ -43,7 +24,6 @@ function App() {
         <Route path='/demo' element={<Demo/>}/>
         <Route path='/productitems' element={<ProductItems/>}/>
       </Routes>
->>>>>>> origin/master
     </div>
    </Router> 
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Collection } from '../components/common/content/Collection';
 import Header from '../components/common/header/Header';
 import { Categories } from '../components/common/category/Categories';
+import Trending from '../components/common/trending/Trending';
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Header/>
       <Collection/>
       <Categories />
+      <Trending />
     </div>
   )
 }
