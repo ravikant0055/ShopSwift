e488585b38a5ffccbc7f081f56b86228cbe6d3ce
Ravi Kant <Kumarra927@gmail.com>
2024-02-09 15:52:32 +0530

    Image zoom apply on product page

--- src/components/common/ProductDetails/ProductDetail.jsx
+++ src/components/common/ProductDetails/ProductDetail.jsx
@@ -1,33 +1,43 @@
 import React from 'react'
 import "./ProductDetail.css"
+import InnerImageZoom from 'react-inner-image-zoom'
+import 'react-inner-image-zoom/lib/InnerImageZoom/styles.css'
 
 const ProductDetail = () => {
   return (
     <div className='product-mainContainer'>
       <div className='product-imgDiv'>
         
-      <div className="top-image">
-        <img src={process.env.PUBLIC_URL + '/images/men.png'} alt="Top Image" />
+      <div className="top-image w-[700px]">
+        <InnerImageZoom 
+          src={process.env.PUBLIC_URL + '/images/men.png'} 
+          zoomSrc={process.env.PUBLIC_URL + '/images/men.png'} 
+          zoomType="hover"
+          hideCloseButton="true"
+          hideHint="true"
+        />
       </div>
+      
       <div className="image-grid">
         <div className="grid-item">
-          <img src={process.env.PUBLIC_URL + '/images/men.png'} alt="Image 1" />
+           <InnerImageZoom src={process.env.PUBLIC_URL + '/images/men.png'} zoomSrc={process.env.PUBLIC_URL + '/images/men.png'} zoomType="hover" hideCloseButton="true" hideHint="true" />
         </div>
         <div className="grid-item">
-          <img src={process.env.PUBLIC_URL + '/images/men.png'} alt="Image 2" />
+           <InnerImageZoom src={process.env.PUBLIC_URL + '/images/men.png'} zoomSrc={process.env.PUBLIC_URL + '/images/men.png'} zoomType="hover" hideCloseButton="true" hideHint="true" />
         </div>
         <div className="grid-item">
-          <img src={process.env.PUBLIC_URL + '/images/men.png'} alt="Image 2" />
+           <InnerImageZoom src={process.env.PUBLIC_URL + '/images/men.png'} zoomSrc={process.env.PUBLIC_URL + '/images/men.png'} zoomType="hover" hideCloseButton="true" hideHint="true" />
         </div>
         <div className="grid-item">
-          <img src={process.env.PUBLIC_URL + '/images/men.png'} alt="Image 2" />
+           <InnerImageZoom src={process.env.PUBLIC_URL + '/images/men.png'} zoomSrc={process.env.PUBLIC_URL + '/images/men.png'} zoomType="hover" hideCloseButton="true" hideHint="true" />
         </div>
         <div className="grid-item">
-          <img src={process.env.PUBLIC_URL + '/images/men.png'} alt="Image 2" />
+           <InnerImageZoom src={process.env.PUBLIC_URL + '/images/men.png'} zoomSrc={process.env.PUBLIC_URL + '/images/men.png'} zoomType="hover" hideCloseButton="true" hideHint="true" />
         </div>
         <div className="grid-item">
-          <img src={process.env.PUBLIC_URL + '/images/men.png'} alt="Image 2" />
+           <InnerImageZoom src={process.env.PUBLIC_URL + '/images/men.png'} zoomSrc={process.env.PUBLIC_URL + '/images/men.png'} zoomType="hover" hideCloseButton="true" hideHint="true" />
         </div>
+        
         
 
       </div>
